bc876c6ec47293fec073b2ddecf4abfec39d2194
Praveen Kumar <praveen+git@kumar.in>
2018-12-02 00:45:28 -0800

    day01b: Compute resulting frequency that repeats first.

--- src/day01.rs
+++ src/day01.rs
@@ -1,4 +1,7 @@
-/// Compute resulting frequency from a list of deltas seperated by a line break.
+use std::collections::HashSet;
+
+/// Compute resulting frequency by summing a list of deltas that are seperated
+/// by a line break.
 ///
 /// # Examples
 ///
@@ -9,8 +12,47 @@
 /// assert_eq!(-3, resulting_frequency("-3"));
 /// assert_eq!(3, resulting_frequency("+1\n+1\n+1"));
 /// assert_eq!(0, resulting_frequency("+1\n+1\n-2"));
-/// assert_eq!(-5, resulting_frequency("-1\n-2\n-3"));
+/// assert_eq!(-6, resulting_frequency("-1\n-2\n-3"));
 /// ```
 pub fn resulting_frequency(input: &str) -> i64 {
-    input.split("\n").map(|s| s.trim()).filter(|s| !s.is_empty()).map(|s| s.parse::<i64>().unwrap()).fold(0, |acc, x| acc + x)
+    input
+        .split("\n")
+        .map(|s| s.trim())
+        .filter(|s| !s.is_empty())
+        .map(|s| s.parse().unwrap())
+        .fold(0, |acc, x: i64| acc + x)
+}
+
+/// Compute the frequency that's reached twice from a list of deltas seperated
+/// by a line break.
+///
+/// # Examples
+///
+/// ```
+/// use aoc18::day01::resulting_frequency_twice;
+///
+/// assert_eq!(0, resulting_frequency_twice("+1\n-1"));
+/// assert_eq!(10, resulting_frequency_twice("+3\n+3\n+4\n-2\n-4"));
+/// assert_eq!(5, resulting_frequency_twice("-6\n+3\n+8\n+5\n-6"));
+/// assert_eq!(14, resulting_frequency_twice("+7\n+7\n-2\n-7\n-4"));
+/// ```
+pub fn resulting_frequency_twice(input: &str) -> i64 {
+    let mut frequencies = HashSet::new();
+    let deltas: Vec<i64> = input
+        .split("\n")
+        .map(|s| s.trim())
+        .filter(|s| !s.is_empty())
+        .map(|s| s.parse().unwrap())
+        .collect();
+    let mut result = 0;
+    frequencies.insert(result);
+    for delta in deltas.iter().cycle() {
+        result += delta;
+        if !frequencies.contains(&result) {
+            frequencies.insert(result);
+        } else {
+            break;
+        }
+    }
+    result
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 extern crate aoc18;
 
+use std::env;
 use std::io::{self, Read};
 
-fn main() {
+fn read_stdin_and_report_result<T: std::fmt::Display>(f: fn(&str) -> T) {
     let mut buffer = String::new();
     match io::stdin().read_to_string(&mut buffer) {
-        Ok(_) => println!("{}", aoc18::day01::resulting_frequency(&buffer)),
+        Ok(_) => println!("{}", f(&buffer)),
         Err(e) => println!("Failed to read from STDIN: {}", e),
+    }
+}
+
+fn main() {
+    let command = &env::args().nth(1).unwrap() as &str;
+    match command {
+        "day01a" => read_stdin_and_report_result(aoc18::day01::resulting_frequency),
+        "day01b" => read_stdin_and_report_result(aoc18::day01::resulting_frequency_twice),
+        _ => println!("Unknown command: {}", command),
     }
 }
